bcc20cb0e8e87273abcc53a0222d897cea7393b0
wadevs14161 <wadevs14161@gmail.com>
2024-05-28 19:24:16 +0800

    db remakemigrations

--- plan/admin.py
+++ plan/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import plan_employee, plan_employee_details
 
 # Register your models here.
+admin.site.register(plan_employee)
+admin.site.register(plan_employee_details)

4dae68153f7352f92606b0129169fcb3490dc304
LUO WEN SHIN <wadevs14161@gmail.com>
2024-05-30 16:03:28 +0800

    add employer_plan model

--- plan/views.py
+++ plan/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .models import plan_employee
+from employer.models import plan_employer
 
 # Create your views here.
 def plans(request):
     plans_employee = plan_employee.objects.all()
+    plans_employer = plan_employer.objects.all()
 
     context = {
-        'plans_employee': plans_employee
+        'plans_employee': plans_employee,
+        'plans_employer': plans_employer
     }
     return render(request, 'plans.html', context)

ec4b9ff834a75c89dcf95f4014ec3111ea3aaaf8
LUO WEN SHIN <wadevs14161@gmail.com>
2024-06-05 11:58:45 +0800

    use javascript to create form

--- employee/forms.py
+++ employee/forms.py
@@ -8,42 +8,44 @@ from .choices import *
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = ['name', 'gender', 'age', 'unemployment_duration']
+        # fields = ['name', 'gender', 'age', 'unemployment_duration']
+        fields = ['unemployment_duration']
 
-    name = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                }),
-        label='姓名')
+    # name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             }),
+    #     label='姓名')
     
-    gender = forms.ChoiceField(choices=choices_gender, widget=forms.RadioSelect(), label='性別', )
+    # gender = forms.ChoiceField(choices=choices_gender, widget=forms.RadioSelect(), label='性別', )
 
-    age = forms.IntegerField(
-        widget=forms.NumberInput(
-            attrs={
-                }),
-        label='年齡')
+    # age = forms.IntegerField(
+    #     widget=forms.NumberInput(
+    #         attrs={
+    #             }),
+    #     label='年齡')
     
     unemployment_duration = forms.ChoiceField(
         choices=choices_unemployment_duration,
         widget=forms.RadioSelect(
             attrs={
+                'class': 'employee-form',
                 }),
         label='失業週期')
     
-    current_status = forms.MultipleChoiceField(
-        choices=choices_current,
-        widget=forms.CheckboxSelectMultiple(
-            attrs={
-                }),
-        label='就業狀況')
+    # current_status = forms.MultipleChoiceField(
+    #     choices=choices_current,
+    #     widget=forms.CheckboxSelectMultiple(
+    #         attrs={
+    #             }),
+    #     label='就業狀況')
     
-    identity = forms.MultipleChoiceField(
-        choices=choices_identity,
-        widget=forms.CheckboxSelectMultiple(
-            attrs={
-                }),
-        label='身份別')
+    # identity = forms.MultipleChoiceField(
+    #     choices=choices_identity,
+    #     widget=forms.CheckboxSelectMultiple(
+    #         attrs={
+    #             }),
+    #     label='身份別')
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -51,12 +53,12 @@ class EmployeeForm(forms.ModelForm):
         self.helper.form_tag = False
         self.helper.layout = Layout(
             # Set all fields css_class to form-label
-            Field('name', css_class='form-label'),
-            Field('gender', css_class='form-label'),
-            Field('age', css_class='form-label'),
-            Field('unemployment_duration', css_class='form-label'),
-            Field('current_status', css_class='form-label'),
-            Field('identity', css_class='form-label'),
+            # Field('name', css_class='form-label'),
+            # Field('gender', css_class='form-label'),
+            # Field('age', css_class='form-label'),
+            # Field('unemployment_duration', css_class='form-label'),
+            # Field('current_status', css_class='form-label'),
+            # Field('identity', css_class='form-label'),
         )
     
     
--- employee/views.py
+++ employee/views.py
@@ -2,58 +2,61 @@ from django.shortcuts import render, HttpResponseRedirect
 from .forms import EmployeeForm
 from .models import Employee, employee_current, employee_identity
 from employer.models import Employer
+from .choices import choices_current
 
 # Create your views here.
 def employee(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
+        # name = request.POST.get('name')
         gender = request.POST.get('gender')
-        age = request.POST.get('age')
-        unemployment_duration = request.POST.get('unemployment_duration')
+        current = request.POST.getlist('current_status')
+        # age = request.POST.get('age')
+        # unemployment_duration = request.POST.get('unemployment_duration')
 
-        # Create the Employee object
-        employee = Employee.objects.create(name=name, gender=gender, age=age, unemployment_duration=unemployment_duration)
+        # # Create the Employee object
+        # employee = Employee.objects.create(name=name, gender=gender, age=age, unemployment_duration=unemployment_duration)
         
-        # If age is 45-65, create employee_identity object with '就服法24-1條之對象'
-        if int(age) >= 45 and int(age) <= 65:
-            employee_identity.objects.create(employee_name=employee, identity='就服法24-1條之對象')
+        # # If age is 45-65, create employee_identity object with '就服法24-1條之對象'
+        # if int(age) >= 45 and int(age) <= 65:
+        #     employee_identity.objects.create(employee_name=employee, identity='就服法24-1條之對象')
         
-        # Create the employee_current object
-        current_status_list = request.POST.getlist('current_status')
+        # # Create the employee_current object
+        # current_status_list = request.POST.getlist('current_status')
 
-        for current_status in current_status_list:
-            employee_current.objects.create(employee_name=employee, current_status=current_status)
+        # for current_status in current_status_list:
+        #     employee_current.objects.create(employee_name=employee, current_status=current_status)
 
-        # Create the employee_identity object
-        identity_list = request.POST.getlist('identity')
+        # # Create the employee_identity object
+        # identity_list = request.POST.getlist('identity')
         
-        # Check if '就服法24-1條之對象' is in the identity_list
-        choices_24_1 = [
-            'Lowincome',
-            'Disability',
-            'Onlyincome',
-            'Aboriginal',
-            'Longterm',
-            'rehabilitation',
-            'Abused',
-            'Rejoinwomen',
-        ]
+        # # Check if '就服法24-1條之對象' is in the identity_list
+        # choices_24_1 = [
+        #     'Lowincome',
+        #     'Disability',
+        #     'Onlyincome',
+        #     'Aboriginal',
+        #     'Longterm',
+        #     'rehabilitation',
+        #     'Abused',
+        #     'Rejoinwomen',
+        # ]
 
-        for identity in identity_list:
-            if identity in choices_24_1:
-                try:
-                    employee_identity.objects.create(employee_name=employee, identity='就服法24-1條之對象')
-                except:
-                    continue
-            else:
-                employee_identity.objects.create(employee_name=employee, identity=identity)
+        # for identity in identity_list:
+        #     if identity in choices_24_1:
+        #         try:
+        #             employee_identity.objects.create(employee_name=employee, identity='就服法24-1條之對象')
+        #         except:
+        #             continue
+        #     else:
+        #         employee_identity.objects.create(employee_name=employee, identity=identity)
 
-        return HttpResponseRedirect('applicants')
+        # return HttpResponseRedirect('applicants')
 
     form_employee = EmployeeForm()
 
     context = {
         'form_employee': form_employee,
+        'choices_current': choices_current,
     }
 
     return render(request, 'formEmployee.html', context)

9d7c88cdd88037840fa96aa3ebe3633f9eb0d9d7
wadevs14161 <wadevs14161@gmail.com>
2024-06-13 23:19:27 +0800

    Update training-courses.js

--- static/training-courses.js
+++ static/training-courses.js
@@ -46,10 +46,81 @@ function find_training_courses() {
         .done(function(data) {
             $("#test-textarea").val(JSON.stringify(data, null, 2));
             thread_id = data["id"];
+            user_question(thread_id);
         })
     });
 }
 
 function user_question(thread_id) {
+    const endpoint = "openai-wenshin.openai.azure.com";
+    var url = `https://${endpoint}/openai/threads/${thread_id}/messages?api-version=2024-02-15-preview`;
+    
+    var message = "我想學烹飪";
+    
+    var parameter = {
+        "role": "user",
+        "content": message
+    };
 
+    $.ajax({
+        url: url,
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("api-key", API_KEY);
+        },
+        type: "POST",
+        data: JSON.stringify(parameter),
+    })
+    // if success, print response in json in textarea with id "test-textarea"
+    .done(function(data) {
+        console.log(data);
+        $("#test-textarea").val(JSON.stringify(data, null, 2));
+        answer_question(thread_id);
+    })
+}
+
+function answer_question(thread_id) {
+    const endpoint = "openai-wenshin.openai.azure.com";
+    var url = `https://${endpoint}/openai/threads/${thread_id}/runs?api-version=2024-02-15-preview`;
+
+    parameter = {
+        "assistant_id": "asst_xdpLb6YW9y83IYN2vA9B0yLc",
+    }
+
+    $.ajax({
+        url: url,
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("api-key", API_KEY);
+        },
+        type: "POST",
+        data: JSON.stringify(parameter),
+    })
+    // if success, print response in json in textarea with id "test-textarea"
+    .done(function(data) {
+        console.log(data);
+        $("#test-textarea").val(JSON.stringify(data, null, 2));
+        assistant_response(thread_id);
+    })
+}
+
+function assistant_response(thread_id) {
+    const endpoint = "openai-wenshin.openai.azure.com";
+    var url = `https://${endpoint}/openai/threads/${thread_id}/messages?api-version=2024-02-15-preview`;
+
+
+
+    $.ajax({
+        url: url,
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("api-key", API_KEY);
+        },
+        type: "GET",
+    })
+    // if success, print response in json in textarea with id "test-textarea"
+    .done(function(data) {
+        console.log(data);
+        $("#test-textarea").val(JSON.stringify(data, null, 2));
+    })
 }

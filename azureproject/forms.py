af0597404b4fc1c3eec69324f9efecf5ede35988
LUO WEN SHIN <wadevs14161@gmail.com>
2024-06-24 14:30:06 +0800

    user-qurey-form for both

--- azureproject/forms.py
+++ azureproject/forms.py
@@ -1,11 +1,33 @@
 from django import forms
 from employee.models import *
+from employer.models import *
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
 class plans_filter_form(forms.Form):
 
-    employee = forms.ModelChoiceField(required=True,
+    def __init__(self, *args, **kwargs):
+        super(plans_filter_form, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        # add id "query-type" to the field "type"
+        self.fields['type'].widget.attrs['id'] = 'query-type'
+
+    type = forms.ChoiceField(required=True,
+                             widget=forms.RadioSelect,
+                                label={
+                                    
+                                },
+                             choices=[('民眾', '民眾'), ('雇主', '雇主')],
+                             )
+
+    employee = forms.ModelChoiceField(required=False,
                                       widget=forms.Select,
+                                      label='登錄代號',
                                       queryset=Employee.objects.all().values_list('name', flat=True),
                                       )
+    
+    employer = forms.ModelChoiceField(required=False,
+                                        widget=forms.Select,
+                                        label='登錄代號',
+                                        queryset=Employer.objects.all().values_list('name', flat=True),
+                                        )
--- azureproject/views.py
+++ azureproject/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import plans_filter_form
 from employee.models import Employee, employee_current, employee_identity
 from plan.models import plan_employee, plan_employee_details
@@ -7,39 +7,46 @@ from itertools import chain
 
 def benefit(request):
     if request.method == 'POST':
-        # Get the employee object
-        name = request.POST.get('employee')
-        employee = Employee.objects.get(name=name)
-        employee_current_list = list(employee_current.objects.values_list('current_status', flat=True).filter(employee_name=employee))
-        employee_identity_list = list(employee_identity.objects.values_list('identity', flat=True).filter(employee_name=employee))
+        type = request.POST.get('type')
+        if type == '民眾':
+            # Get the employee object
+            name = request.POST.get('employee')
+            if not name:
+                return redirect('/benefit')
+            employee = Employee.objects.get(name=name)
+            employee_current_list = list(employee_current.objects.values_list('current_status', flat=True).filter(employee_name=employee))
+            employee_identity_list = list(employee_identity.objects.values_list('identity', flat=True).filter(employee_name=employee))
 
-        # Get the plan_employee object
-        # Filter the plan_employee object by current_status list, gender and age
-        plan_employee_list1 = plan_employee.objects.filter(required_employee_current__in=employee_current_list)
+            # Get the plan_employee object
+            # Filter the plan_employee object by current_status list, gender and age
+            plan_employee_list1 = plan_employee.objects.filter(required_employee_current__in=employee_current_list)
+            
+            if employee.gender == 'Male':
+                plan_employee_list1 = plan_employee_list1.exclude(required_employee_gender='female')
+            
+            plan_employee_list1 = plan_employee_list1.filter(employee_age_lower_bound__lte=employee.age)
+            plan_employee_list1 = plan_employee_list1.filter(employee_age_upper_bound__gte=employee.age)
+
+            # Filter the plan_employee object by identity list
+
+            # Concatenate the plan_employee objects
+
+            # Get the plan_employee_details object
+            # Filter the plan_employee_details object by plan_employee object
+            all_plan_employee_details_list = plan_employee_details.objects.all()
+
+            context = {
+                'employee': employee,
+                'employee_current_list': employee_current_list,
+                'employee_identity_list': employee_identity_list,
+                'plan_employee_list1': plan_employee_list1,
+                'all_plan_employee_details_list': all_plan_employee_details_list,
+            }
+
+            return render(request, 'benefit.html', context)
         
-        if employee.gender == 'Male':
-            plan_employee_list1 = plan_employee_list1.exclude(required_employee_gender='female')
-        
-        plan_employee_list1 = plan_employee_list1.filter(employee_age_lower_bound__lte=employee.age)
-        plan_employee_list1 = plan_employee_list1.filter(employee_age_upper_bound__gte=employee.age)
-
-        # Filter the plan_employee object by identity list
-
-        # Concatenate the plan_employee objects
-
-        # Get the plan_employee_details object
-        # Filter the plan_employee_details object by plan_employee object
-        all_plan_employee_details_list = plan_employee_details.objects.all()
-
-        context = {
-            'employee': employee,
-            'employee_current_list': employee_current_list,
-            'employee_identity_list': employee_identity_list,
-            'plan_employee_list1': plan_employee_list1,
-            'all_plan_employee_details_list': all_plan_employee_details_list,
-        }
-
-        return render(request, 'index.html', context)
+        elif type == '雇主':
+            pass
 
     user_qurey_form = plans_filter_form()
 

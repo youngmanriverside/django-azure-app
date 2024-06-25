b2bd3f0f4436eb75fc890a9d91dde616971768a1
LUO WEN SHIN <wadevs14161@gmail.com>
2024-06-25 14:25:26 +0800

    update form

--- employee/forms.py
+++ employee/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, Layout, Field
+from crispy_forms.layout import Submit, Layout, Field, Fieldset
 from crispy_forms.bootstrap import StrictButton, InlineField
 from .models import *
 from .choices import *
@@ -8,15 +8,16 @@ from .choices import *
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = '__all__'
-        # fields = ['name', 'gender', 'age', 'unemployment_duration']
-        # fields = ['unemployment_duration']
+        # fields = '__all__'
+        fields = ['name', 'gender', 'age', 'current_status', 'identity', 'unemployment_duration']
+
+    field_order = ['name', 'gender', 'age', 'current_status', 'identity', 'unemployment_duration']
 
     name = forms.CharField(
         widget=forms.TextInput(
             attrs={
                 }),
-        label='代號')
+        label='姓名')
     
     gender = forms.ChoiceField(choices=choices_gender, widget=forms.RadioSelect(), label='性別', )
 
@@ -25,21 +26,14 @@ class EmployeeForm(forms.ModelForm):
             attrs={
                 }),
         label='年齡')
-    
-    unemployment_duration = forms.ChoiceField(
-        choices=choices_unemployment_duration,
-        widget=forms.RadioSelect(
-            attrs={
-                'class': 'employee-form',
-                }),
-        label='失業週期')
-    
-    education = forms.ChoiceField(
-        choices=choices_education,
-        widget=forms.Select(
-            attrs={
-                }),
-        label='學歷')
+        
+    # education = forms.ChoiceField(
+    #     required=False,
+    #     choices=choices_education,
+    #     widget=forms.Select(
+    #         attrs={
+    #             }),
+    #     label='學歷')
     
     current_status = forms.MultipleChoiceField(
         choices=choices_current,
@@ -54,6 +48,16 @@ class EmployeeForm(forms.ModelForm):
             attrs={
                 }),
         label='身份別')
+    
+    unemployment_duration = forms.ChoiceField(
+        required=False,
+        choices=choices_unemployment_duration,
+        widget=forms.RadioSelect(
+            attrs={
+                'class': 'employee-form',
+                }),
+        label='失業週期')
+    
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
